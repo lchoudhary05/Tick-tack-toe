89312b18281636362842e4aff44a5024713ae511
lchoudhary <mails4lavesh@gmail.com>
2025-07-10 20:14:28 -0700

    working fine

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Cell from "./Cell";
+import "./Cell.css";
 
-export default function Board() {
+export default function App() {
   const [turn, setTurn] = useState("X");
-  let item = [1, 3, 4];
+  const [winner, setWinner] = useState(null);
+  const [values, setValues] = useState(Array(9).fill(null));
+  useEffect(() => {
+    findWinner();
+  }, [values]);
+  function handleMove(i) {
+    if (values[i] || winner) {
+      return;
+    }
+    let newValues = values.slice();
+    newValues[i] = turn;
+    setValues(newValues);
+    if (turn == "X") {
+      setTurn("O");
+    } else {
+      setTurn("X");
+    }
+  }
+  function restart() {
+    setValues(Array(9).fill(null));
+    setTurn("X");
+    setWinner(null);
+  }
+  function findWinner() {
+    for (let x = 0; x < winnerTable.length; x++) {
+      const [a, b, c] = winnerTable[x];
+      if (values[a] && values[a] == values[b] && values[b] == values[c]) {
+        setWinner(values[a]);
+        return values[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
-      <h3>Your Turn: {turn}</h3>
-      {item.map((_) => (
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          {item.map((_) => (
-            <Cell curr={turn} setCurr={setTurn} />
-          ))}
-        </div>
-      ))}
+      {!winner && <h3>Next Turn:{turn}</h3>}
+      {winner && <h3>Winner:{winner}</h3>}
+
+      <div className="grid-row">
+        <Cell value={values[0]} setSquare={() => handleMove(0)} />
+        <Cell value={values[1]} setSquare={() => handleMove(1)} />
+        <Cell value={values[2]} setSquare={() => handleMove(2)} />
+      </div>
+      <div className="grid-row">
+        <Cell value={values[3]} setSquare={() => handleMove(3)} />
+        <Cell value={values[4]} setSquare={() => handleMove(4)} />
+        <Cell value={values[5]} setSquare={() => handleMove(5)} />
+      </div>
+      <div className="grid-row">
+        <Cell value={values[6]} setSquare={() => handleMove(6)} />
+        <Cell value={values[7]} setSquare={() => handleMove(7)} />
+        <Cell value={values[8]} setSquare={() => handleMove(8)} />
+      </div>
+      <button onClick={restart}>Restart</button>
     </>
   );
 }
+
+const winnerTable = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,26 +1,8 @@
-import { useState } from "react";
-
+import "./Cell.css";
 export default function Cell(props) {
-  const [present, setPresent] = useState("");
-  function handleClick() {
-    if (props.curr === "X") props.setCurr("O");
-    else props.setCurr("X");
-
-    setPresent(props.curr);
-  }
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "#c6c8cc",
-          width: "50px",
-          height: "60px",
-          borderStyle: "solid",
-        }}
-        onClick={handleClick}
-      >
-        {present}
-      </div>
-    </>
+    <button className="square" onClick={props.setSquare}>
+      {props.value}
+    </button>
   );
 }
